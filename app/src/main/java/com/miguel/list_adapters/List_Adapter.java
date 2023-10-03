d39f59603cec6593a9c9e1483fa24cba109f360d
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2023-10-02 19:30:33 -0500

    commit antes del descanso

--- app/src/main/java/com/miguel/list_adapters/List_Adapter.java
+++ app/src/main/java/com/miguel/list_adapters/List_Adapter.java
@@ -2,7 +2,10 @@ package com.miguel.list_adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
 public class List_Adapter extends BaseAdapter {
     Context contexto; //en donde estamos en este momento, en que pantalla
@@ -18,9 +21,25 @@ public class List_Adapter extends BaseAdapter {
         this.numero_telfono = numero_telfono;
         this.pais = pais;
         this.id_imagen = id_imagen;
+        inflater = LayoutInflater.from(contexto);
     }
 
-    @Override asfdasdfasdfsadf
+    @Override
+    public int getCount(){return nombres.length;} //devuelve la cantidad de datos recibidos
+
+    @Override
+    public Object getItem(int i){return null;}  //devuelve un objeto vacio y recibe la cantidad de datos
+
+    @Override
+    public long getItemId(int i){return 0;}  //el id siempre va a ser 0
+    @Override
+    public View getView(int i, View vista, ViewGroup viewGroup){ //obtener la vista
+
+        vista = inflater.inflate(R.layout.list_tem, null);
+        ImageView fotoPerfil = vista.findViewById(R.id)
+
+        return vista;
+    }
 
 
 
